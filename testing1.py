42864d16e18a22bb7d7eecc5798f2e96c1121ded
Nathan Dalpatan <st23453@ormiston.school.nz>
2023-06-19 10:11:51 +1200

    Create testing1.py
    Creating the form for the first time

---
+++ testing1.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import filedialog, Text
+import os
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, height=700, width=700, bg="#121212")
+canvas.pack()
+
+root.mainloop()
