730e42d1e2668a9fa3d034e903ce4ee08322079a
Nathan Dalpatan <st23453@ormiston.school.nz>
2023-06-21 17:38:57 +1200

    Update testing1.py

--- testing1.py
+++ testing1.py
@@ -1,10 +1,34 @@
 import tkinter as tk
-from tkinter import filedialog, Text
-import os
+from tkinter import ttk
 
-root = tk.Tk()
+def convert():
+    print(entry_int.get())
 
-canvas = tk.Canvas(root, height=700, width=700, bg="#121212")
-canvas.pack()
+# window
+window = tk.Tk()
+window.title('Demo')
+window.geometry('300x150')
 
-root.mainloop()
+#title
+title_label = ttk.Label(master = window, text = 'WELCOME TO CALORIE BURNER', font = 'Helvetica 24 bold')
+title_label.pack()
+
+# input field
+input_frame = ttk.Frame(master = window)
+entry_int = tk.IntVar()
+entry = ttk.Entry(master = input_frame, textvariable = entry_int)
+button = ttk.Button(master = input_frame, text = 'Sign in', command = convert)
+entry.pack(side = 'left', padx = 10)
+button.pack(side = 'left')
+input_frame.pack(pady = 10)
+
+#output
+output_label = tk.StringVar()
+output_label = ttk.Label(
+    master = window,
+    text = 'Output',
+    font = 'Calibri 24')
+output_label.pack(pady = 5)
+
+# run
+window.mainloop()
